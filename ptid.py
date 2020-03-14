1dd13310e9bc1165c8cebae51a6af4c1431c484c
dan-rds <ddrichar@ucsc.edu>
2020-03-14 13:19:19 -0700

    linting and about to try to sub-function somestuff in ptid

--- ptid.py
+++ ptid.py
@@ -2,148 +2,188 @@
 ptid Peak-Trough-Incline-Decline algorithm
 
 """
-import ephem
+
 import datetime
 import copy
 import math
-from math import pi
-
-from observatory import Observatory
-from pprint import pprint
 import inspect
+from math import pi
+import ephem
+from pprint import pprint
+from typing import Tuple, Type
+from observatory import Observatory
+from target_timeseries import TargetTimeSeries
+
 
 extrema_pt = None
+
+
 def peek(x):
     """ quick helper util to know wtf is happening with these ephem objects"""
     pprint(inspect.getmembers(x))
 
-def distance(a_coord, b_coord, ts=None) -> float:
-	a_alt, a_az = a_coord
-	b_alt, b_az = b_coord
 
-	if a_az < 0 or b_az < 0: 
-		raise ValueError
-	if abs(a_alt) > pi or abs(b_alt) > pi:
-		raise ValueError
-	az_diff = abs(a_az - b_az)
-	if az_diff > pi:
-		#print("WRAPPING AZMUTH")
-		az_diff = (2*pi) - az_diff
+def distance(a_coord: Tuple[float, float], b_coord: Tuple[float, float]) -> float:
+    ''' Angular distance between two points '''
+    a_alt, a_az = a_coord
+    b_alt, b_az = b_coord
 
-	alt_diff = abs(a_alt - b_alt)
+    if a_az < 0 or b_az < 0:
+        raise ValueError
+    if abs(a_alt) > pi or abs(b_alt) > pi:
+        raise ValueError
+    az_diff = abs(a_az - b_az)
+    if az_diff > pi:
+        az_diff = (2 * pi) - az_diff
 
-#	print(ephem.degrees(a_alt),ephem.degrees(a_az), ts)
-	#return (alt_diff, az_diff)
-	dist = math.sqrt(alt_diff**2 + az_diff**2)
-	return dist
+    alt_diff = abs(a_alt - b_alt)
 
-def is_increasing_slope(left_index, right_index, observatory, sat, target_timeseries) -> (bool, float):
-	
-	left_aa, left_sampletime = target_timeseries.get_target_aa(left_index)
-	right_aa, right_sampletime = target_timeseries.get_target_aa(right_index)
-#	print(type(left_sampletime))
-	observatory.date = left_sampletime
-#	print("OBS DATE: ", observatory.date)
-	sat.compute(observatory)
-	sat_aa = (sat.alt, sat.az)
-	dist_sat_to_left_sample = distance(a_coord=sat_aa, b_coord=left_aa)
-
-	
-	observatory.date = right_sampletime
-	sat.compute(observatory)
-	sat_aa = (sat.alt, sat.az)
-	dist_sat_to_right_sample = distance(a_coord=sat_aa, b_coord=right_aa)
+    dist = math.sqrt(alt_diff**2 + az_diff**2)
+    return dist
 
 
-	return dist_sat_to_left_sample < dist_sat_to_right_sample, min(dist_sat_to_left_sample, dist_sat_to_right_sample)
+def is_increasing_slope(left_index: int, right_index: int,
+                        observatory: Type[Observatory],
+                        sat: Type[ephem.Body],
+                        target_timeseries: Type[TargetTimeSeries]) -> Tuple[bool, float]:
+
+    left_aa, left_sampletime = target_timeseries.get_target_aa(left_index)
+    right_aa, right_sampletime = target_timeseries.get_target_aa(right_index)
+
+    observatory.date = left_sampletime
+
+    sat.compute(observatory)
+    sat_aa = (sat.alt, sat.az)
+    dist_sat_to_left_sample = distance(a_coord=sat_aa, b_coord=left_aa)
+
+    observatory.date = right_sampletime
+    sat.compute(observatory)
+    sat_aa = (sat.alt, sat.az)
+    dist_sat_to_right_sample = distance(a_coord=sat_aa, b_coord=right_aa)
+
+    return dist_sat_to_left_sample < dist_sat_to_right_sample, min(
+        dist_sat_to_left_sample, dist_sat_to_right_sample)
 
 
-def sanity_check(observatory, sat, target_timeseries, color='b--', min_to_draw=None):
-	import matplotlib.pyplot as plt
-	global extrema_pt
+def sanity_check(observatory, sat, target_timeseries,
+                 color='b--', min_to_draw=None):
+    import matplotlib.pyplot as plt
+    global extrema_pt
 
-	dist_list = []
-	tar_list = []
-	sat_list = []
+    dist_list = []
+    tar_list = []
+    sat_list = []
 
-	start = target_timeseries.start_datetime
-	target = target_timeseries.target_body
+    start = target_timeseries.start_datetime
+    target = target_timeseries.target_body
 
-	dt = None
-	for i in range(target_timeseries.len()):
-		dt = datetime.timedelta(seconds=i)
-		observatory.date = start + dt
-		
-		sat.compute(observatory)
-		target.compute(observatory)
-		sat_aa = (sat.alt, sat.az)
-		
-		tar_aa = (target.alt, target.az)
-		d = distance(a_coord=sat_aa, b_coord=tar_aa)
-		dist_list.append(d)
+    dt = None
+    for i in range(target_timeseries.len()):
+        dt = datetime.timedelta(seconds=i)
+        observatory.date = start + dt
 
-		sat_list.append(sat_aa)
+        sat.compute(observatory)
+        target.compute(observatory)
+        sat_aa = (sat.alt, sat.az)
 
-	plt.figure()
-	plt.title(sat.name)
-	print(dt)
+        tar_aa = (target.alt, target.az)
+        d = distance(a_coord=sat_aa, b_coord=tar_aa)
+        dist_list.append(d)
+
+        sat_list.append(sat_aa)
+
+    plt.figure()
+    plt.title(sat.name)
+    print(dt)
+
+    if min_to_draw:
+        print("MTD=", min_to_draw)
+        plt.axhline(y=min_to_draw, color='r', linestyle='-')
+        plt.axhline(y=extrema_pt[1], color='b', linestyle='--')
+        plt.axvline(x=extrema_pt[0], color='b', linestyle='--')
+        #plt.scatter(extrema_pt[0], extrema_pt[1], s=200)
+    plt.plot(dist_list, color)
+    plt.ylabel('Distache to target')
+    plt.show()
 
 
-	if min_to_draw:
-		print("MTD=", min_to_draw)
-		plt.axhline(y=min_to_draw, color='r', linestyle='-')
-		plt.axhline(y=extrema_pt[1], color='b', linestyle='--')
-		plt.axvline(x=extrema_pt[0], color='b', linestyle='--')
-		#plt.scatter(extrema_pt[0], extrema_pt[1], s=200)
-	plt.plot(dist_list, color)
-	plt.ylabel('Distache to target')
-	plt.show()
+def is_minima_index(mid_index: int,
+                    observatory: Type[Observatory],
+                    sat: Type[ephem.Body],
+                    target_timeseries: Type[TargetTimeSeries]) -> (bool, float):
 
-def is_minima_index(mid_index, observatory, sat, target_timeseries) -> (bool, float):
-	#todo speedup with returning slope
-	left_slope, left_val = is_increasing_slope(left_index = mid_index-1, right_index=mid_index, observatory=observatory, sat=sat, target_timeseries=target_timeseries)
-	right_slope, right_val = is_increasing_slope(left_index = mid_index, right_index=mid_index+1, observatory=observatory, sat=sat, target_timeseries=target_timeseries)
-	return (left_slope == False and right_slope == True), min(left_val, right_val)
+    left_slope, left_val = is_increasing_slope(left_index=mid_index - 1,
+                                               right_index=mid_index,
+                                               observatory=observatory,
+                                               sat=sat,
+                                               target_timeseries=target_timeseries)
+    right_slope, right_val = is_increasing_slope(left_index=mid_index,
+                                                 right_index=mid_index + 1,
+                                                 observatory=observatory,
+                                                 sat=sat,
+                                                 target_timeseries=target_timeseries)
+    found_minima = (left_slope == False and right_slope == True)
+    return found_minima, min(left_val, right_val)
+
 
 def ptid_rec(lo, hi, observatory, sat, target_timeseries) -> float:
-	global extrema_pt
+    global extrema_pt
 
-	mid_index = lo + (hi - lo)//2
+    mid_index = lo + (hi - lo) // 2
 
+    break_cond, distance = is_minima_index(
+        mid_index=mid_index, observatory=observatory, sat=sat, target_timeseries=target_timeseries)
+    if break_cond or mid_index == hi or mid_index == lo:
+        #print("MINIMA dist = ", distance)
+        extrema_pt = (mid_index, distance)
 
-	break_cond, distance = is_minima_index(mid_index, observatory=observatory, sat=sat, target_timeseries=target_timeseries)
-	if break_cond or mid_index == hi or mid_index == lo:
-		#print("MINIMA dist = ", distance)
-		extrema_pt = (mid_index, distance)
+        return distance
 
-		return distance
-
-	mid_positive_slope, val = is_increasing_slope(left_index=mid_index, right_index=mid_index+1, observatory=observatory, sat=sat, target_timeseries=target_timeseries)
-	if mid_positive_slope:
-		rec_val =  ptid_rec(lo, mid_index, observatory, sat, target_timeseries)
-	else:
-		rec_val = ptid_rec(mid_index+1, hi, observatory, sat, target_timeseries) #todo rm +1??
+    mid_positive_slope, val = is_increasing_slope(
+        left_index=mid_index, right_index=mid_index + 1, observatory=observatory, sat=sat, target_timeseries=target_timeseries)
+    if mid_positive_slope:
+        rec_val = ptid_rec(lo, mid_index, observatory, sat, target_timeseries)
+    else:
+        rec_val = ptid_rec(
+            mid_index + 1,
+            hi,
+            observatory,
+            sat,
+            target_timeseries)  # todo rm +1??
 #	print(min(rec_val, val), rec_val, val,  type(min(rec_val, val)))
-	return min(rec_val, val)
+    return min(rec_val, val)
+
+
 def ptid(observatory, sat, target_timeseries):
-	#observatory = Observatory('config.yaml')
+    #observatory = Observatory('config.yaml')
 
-	start_positive_slope, start_min = is_increasing_slope(left_index=0, right_index=1, observatory=observatory, sat=sat, target_timeseries=target_timeseries)
-	end_positive_slope, end_min = is_increasing_slope(left_index = -2, right_index=-1, observatory=observatory, sat=sat, target_timeseries=target_timeseries)
-# start_positive_slope, start_min = is_increasing_slope(left_index=0, right_index=1, observatory=observatory, sat=sat, target_timeseries=target_timeseries)
-# 	end_positive_slope, end_min = is_increasing_slope(left_index = -2, right_index=-1, observatory=observatory, sat=sat, target_timeseries=target_timeseries)
+    start_positive_slope, start_min = is_increasing_slope(
+        left_index=0, right_index=1, observatory=observatory, sat=sat, target_timeseries=target_timeseries)
+    end_positive_slope, end_min = is_increasing_slope(
+        left_index=-2, right_index=-1, observatory=observatory, sat=sat, target_timeseries=target_timeseries)
 
-	peak_val = float('inf')
+    peak_val = float('inf')
 
-	if start_positive_slope == False != end_positive_slope == True:
+    if start_positive_slope == False != end_positive_slope == True:
 
-		print("Peak", sat.name)
-		peak_val = ptid_rec(0, (target_timeseries.len()-1), observatory, sat, target_timeseries)
-		#if peak_val < min(end_min, start_min):
-		sanity_check(observatory=observatory, sat=sat, target_timeseries=target_timeseries, min_to_draw=min(peak_val, end_min, start_min), color='g-')
-	
-	#sanity_check(observatory=observatory, sat=sat, target_timeseries=target_timeseries)
-	return min(peak_val, end_min, start_min)
-		
+        
+        peak_val = ptid_rec(
+            0,
+            (target_timeseries.len() - 1),
+            observatory,
+            sat,
+            target_timeseries)
+        print("Peak", sat.name, peak_val)
+        # if peak_val < min(end_min, start_min):
+        # sanity_check(
+        #     observatory=observatory,
+        #     sat=sat,
+        #     target_timeseries=target_timeseries,
+        #     min_to_draw=min(
+        #         peak_val,
+        #         end_min,
+        #         start_min),
+        #     color='g-')
 
-
+    #sanity_check(observatory=observatory, sat=sat, target_timeseries=target_timeseries)
+    return min(peak_val, end_min, start_min)
--- target_timeseries.py
+++ target_timeseries.py
@@ -1,63 +1,91 @@
-import copy
-import ephem
-import yaml
-import pyproj
+
+'''
+-------------------------------------
+target_timeseries
+find_sat_flybys
+
+    TargetTimeSeries functions as a cache for the target's
+    position (alt , az) so that the position doesn't need to
+    be frequently recalculated.
+
+    by Daniel Richards (github.com/dan-rds)
+    Copyright 2020 Daniel Richards. All rights reserved.
+--------------------------------------
+'''
+
 from pprint import pprint
 import inspect
-from observatory import Observatory
-import sys
 import datetime
-from typing import Tuple, List, Type
+import ephem
+from observatory import Observatory
+
+from typing import Tuple, Type
+
+
 def peek(x):
     """ quick helper util to know wtf is happening with these ephem objects"""
     pprint(inspect.getmembers(x))
 
-        
 
-class TargetTimeSeries(object):
+class TargetTimeSeries():
     """
     A class to find and store the target's position for a given time.
     Only calculating said position when needed
     """
-    def __init__(self, observatory: Type[Observatory], target_ra_dec: Tuple[float,float], start_datetime: Type[datetime.datetime], duration_ms: float):
+
+    def __init__(self, observatory: Type[Observatory],
+                 target_ra_dec: Tuple[float, float],
+                 start_datetime: Type[datetime.datetime],
+                 duration_ms: float):
         """
         TargetTimeSeries constructor
         Args:
             observarory (Observatory): Where observation is taking place
+            start_datetime (datetime.datetime): start of the observation
+            duration_ms: (float): how long the observation will last
             target_ra_dec (tuple): ra and dec of the target of the observation
         Returns:
             TargetTimeSeries
         """
         super().__init__()
-        '''   
+        '''
         print("observatory  [" ,type(observatory).__name__, "]   :", observatory)
         print("target_ra_dec  [" ,type(target_ra_dec).__name__, "]   :", target_ra_dec)
         print("start_time  [" ,type(start_time).__name__, "]   :", start_time)
-        print("duration_ms  [" ,type(duration_ms).__name__, "]   :", duration_ms)   
+        print("duration_ms  [" ,type(duration_ms).__name__, "]   :", duration_ms)
         '''
         self.index_target_map = {}
         self.start_datetime = start_datetime
         self.duration_ms = int(duration_ms)
         self.observatory = observatory
-        target_ra, target_dec = target_ra_dec 
+        target_ra, target_dec = target_ra_dec
         target_body = ephem.FixedBody()
-        target_body._ra= target_ra
-        target_body._dec = target_dec 
+        target_body._ra = target_ra
+        target_body._dec = target_dec
         self.target_body = target_body
         self.cache_misses = 0
         self.cache_hits = 0
 
-    def index_to_date(self, index) -> datetime.datetime:
-        #every second for now
+    def index_to_date(self, index: int) -> datetime.datetime:
+        '''
+            Converts a timeseries index to a datetime object of when
+            that index in the timeseries was calculated.
+            e.g.
+            index=1 would rturn the datetime of one second ofthe the observation started
+            index=-1 would rturn the datetime of the last second of the observation
+
+        '''
         if index < 0:
-        #    tmp = index
-            index = self.duration_ms//1000 + index
-        #    print("old index: ", tmp, "   new index: ", index)
+            index = self.duration_ms // 1000 + index
         after_start = datetime.timedelta(seconds=index)
         return self.start_datetime + after_start
 
     def get_target_aa(self, index: int) -> Tuple[Tuple[float, float], datetime.datetime]:
-        if index in self.index_target_map:
+        '''
+            Get the index-th sample in the timeseries. THis fuctions as a
+            query and insert for the central cache for the timeseries.
+        '''
+        if index in self.index_target_map:  # cache hit
             self.cache_hits += 1
             return self.index_target_map[index], self.index_to_date(index)
         self.cache_misses += 1
@@ -69,23 +97,9 @@ class TargetTimeSeries(object):
         return target_aa, self.index_to_date(index)
 
     def len(self):
-        return self.duration_ms//1000
+        ''' len() safer than an override '''
+        return self.duration_ms // 1000
 
     def cache_hitrate(self):
-        print("hits: ", self.cache_hits,"  misses: ", self.cache_misses)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        ''' Reports cache hit rates '''
+        print("hits: ", self.cache_hits, "  misses: ", self.cache_misses)
